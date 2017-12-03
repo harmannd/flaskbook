2626f603485e8c891facc189fef1094dc5d091e3
harmannd <derekhar2468@gmail.com>
2017-12-03 13:43:27 -0600

    Added profile and edit profile views and forms

--- flaskbook/user/forms.py
+++ flaskbook/user/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import Form
 from wtforms import validators, StringField, PasswordField
+from wtforms.widgets import TextArea
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError
 import re
 
 from user.models import User
 
-class RegisterForm(Form):
+class BaseUserForm(Form):
     first_name = StringField('First Name', [validators.DataRequired()])
     last_name = StringField('Last Name', [validators.DataRequired()])
     email = EmailField('Email address', [
@@ -18,23 +19,29 @@ class RegisterForm(Form):
         validators.DataRequired(),
         validators.length(min=4, max=25)
         ])
+    bio = StringField('Bio',
+        widget=TextArea(),
+        validators=[validators.length(max=160)]
+        )
+
+class RegisterForm(BaseUserForm):
     password = PasswordField('New Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match'),
         validators.length(min=4, max=80)
         ])
     confirm = PasswordField('Repeat Password')
-    
+
     def validate_username(form, field):
         if User.objects.filter(username=field.data).first():
             raise ValidationError("Username already exists")
         if not re.match("^[a-zA-Z0-9_-]{4,25}$", field.data):
             raise ValidationError("Invalid username")
-            
+
     def validate_email(form, field):
         if User.objects.filter(email=field.data).first():
             raise ValidationError("Email is already in use")
-            
+
 class LoginForm(Form):
     username = StringField('Username', [
         validators.DataRequired(),
@@ -44,3 +51,6 @@ class LoginForm(Form):
         validators.DataRequired(),
         validators.length(min=4, max=80)
         ])
+
+class EditForm(BaseUserForm):
+    pass
